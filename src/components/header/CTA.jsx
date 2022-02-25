38a3c7ae9c51af8e32f2be663813870e47ba2c82
unknown <subhudas1707@gmail.com>
2022-02-25 14:36:05 +0530

    pdf link

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const CTA = () => {
-  const pdf = "https://SD170720000.github.io/src/assets/subhankar_das_resume.pdf"
+  const pdf = "https://SD170720000.github.io/Subhu_E-Portfolio/src/assets/subhankar_das_resume.pdf"
   return (
     <div className='cta'>
       <a href={pdf} id='resume' className='btn' download="Subhankar Das(Resume)" target='_blank'>Download CV</a>
