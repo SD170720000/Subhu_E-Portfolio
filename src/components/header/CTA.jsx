b6258561c9881d8cd4e928251031b8e7f2b4f0d6
unknown <subhudas1707@gmail.com>
2022-02-25 14:14:25 +0530

    changed links

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import PDF from '../../assets/subhankar_das_resume.pdf'
 
 const CTA = () => {
+  const pdf = "https://raw.githubusercontent.com/SD170720000/Subhu_E-Portfolio/master/src/assets/subhankar_das_resume.pdf"
   return (
     <div className='cta'>
-      <a href={PDF} id='resume' className='btn' download>Download CV</a>
+      <a href={pdf} id='resume' className='btn' download="Subhankar Das(Resume)">Download CV</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
